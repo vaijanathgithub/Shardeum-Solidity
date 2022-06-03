cd2123afda5f71dcb4af60be4f4693560cc5d259
Vaijanath Hiremath <99311748+vaijanathgithub@users.noreply.github.com>
2022-06-03 15:50:03 +0530

    Update ShardeumERC20Token.sol

--- ShardeumERC20Token.sol
+++ ShardeumERC20Token.sol
@@ -6,10 +6,24 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract ShardeumERC20Token is ERC20 {
-  
-  //Pass Token Name & Symbol while deploying this contract
-   constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
-       _mint(msg.sender, 10000 * 10 ** 18);
+    /**
+     * @dev creates & calls ERC20 constructor 
+     *
+     */
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol){
+       _mint(msg.sender, _totalSupply * 10 ** 18);
+    }
 
-   }
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferTokens(address recipient, uint256 amount) external returns (bool){
+        return transfer(recipient, amount);             
+
+    }
+    
 }
